e9b449e17e40461df2e474c25d9e39c6c62ebfef
milicaamladenovic <mi21349@alas.matf.bg.ac.rs>
2024-04-10 19:19:52 +0200

    advanced lighting

--- resources/shaders/2.model_lighting.fs
+++ resources/shaders/2.model_lighting.fs
@@ -27,6 +27,8 @@ uniform PointLight pointLight;
 uniform Material material;
 
 uniform vec3 viewPosition;
+uniform bool blinn;
+
 // calculates the color when using a point light.
 vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
 {
@@ -35,20 +37,25 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
     float diff = max(dot(normal, lightDir), 0.0);
     // specular shading
     vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 halfwayDir = normalize(lightDir + viewDir);
-    spec = pow(max(dot(normal, halfwayDir),0.0), material.shininess);
+    float spec=0.0f;
+    if(blinn){
+        vec3 halfwayDir = normalize(lightDir + viewDir);
+        spec = pow(max(dot(normal, halfwayDir), 0.0), material.shininess);
+    }
+    else{
+        spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    }
     // attenuation
     float distance = length(light.position - fragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
     // combine results
-    vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
+    vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords).rgb);
     vec4 diffSample = texture(material.texture_diffuse1, TexCoords);
     if (diffSample.a < 0.1) {
         discard;
     }
     vec3 diffuse = light.diffuse * diff * vec3(diffSample);
-    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords).xxx);
+    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords).rgb);
 
     ambient *= attenuation;
     diffuse *= attenuation;
