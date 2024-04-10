a02341e0f3ee48bbc9febd91e0f0ab47715457fe
milicaamladenovic <mi21349@alas.matf.bg.ac.rs>
2024-04-10 18:21:04 +0200

    advanced lighting

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,8 @@ unsigned int loadCubemap(vector<std::string> faces);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+bool blinn = false;
+bool blinnKeyPressed = false;
 
 // camera
 
@@ -103,66 +105,14 @@ int main() {
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
+    //blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     // build and compile shaders
     // -------------------------
     Shader ourShader("resources/shaders/2.model_lighting.vs", "resources/shaders/2.model_lighting.fs");
     Shader skyboxShader("resources/shaders/skybox.vs", "resources/shaders/skybox.fs");
-
-//    Shader shader("resources/shaders/3.1.3.shadow_mapping.vs", "resources/shaders/3.1.3.shadow_mapping.fs");
-//    Shader simpleDepthShader("resources/shaders/3.1.3.shadow_mapping_depth.vs", "resources/shaders/3.1.3.shadow_mapping_depth.fs");
-//    Shader debugDepthQuad("resources/shaders/3.1.3.debug_quad.vs", "resources/shaders/3.1.3.debug_quad_depth.fs");
-//
-//
-//    float planeVertices[] = {
-//            // positions            // normals         // texcoords
-//            25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
-//            -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
-//            -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
-//
-//            25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
-//            -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
-//            25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
-//    };
-//
-//    // plane VAO
-//    unsigned int planeVBO;
-//    glGenVertexArrays(1, &planeVAO);
-//    glGenBuffers(1, &planeVBO);
-//    glBindVertexArray(planeVAO);
-//    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
-//    glEnableVertexAttribArray(0);
-//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-//    glEnableVertexAttribArray(1);
-//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-//    glEnableVertexAttribArray(2);
-//    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-//    glBindVertexArray(0);
-//
-//    unsigned int woodTexture = loadTexture(FileSystem::getPath("resources/textures/skyboxgrass/negy.jpg").c_str());
-
-//    // configure depth map FBO
-//    // -----------------------
-//    const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
-//    unsigned int depthMapFBO;
-//    glGenFramebuffers(1, &depthMapFBO);
-//    // create depth texture
-//    unsigned int depthMap;
-//    glGenTextures(1, &depthMap);
-//    glBindTexture(GL_TEXTURE_2D, depthMap);
-//    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-//    float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
-//    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
-//    // attach depth texture as FBO's depth buffer
-//    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-//    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
-//    glDrawBuffer(GL_NONE);
-//    glReadBuffer(GL_NONE);
-//    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 
     // load models
@@ -300,39 +250,12 @@ int main() {
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-//        glm::mat4 lightProjection, lightView;
-//        glm::mat4 lightSpaceMatrix;
-//        float near_plane = 1.0f, far_plane = 7.5f;
-//        //lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
-//        lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
-//        lightView = glm::lookAt(pointLight.position, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
-//        lightSpaceMatrix = lightProjection * lightView;
-//        // render scene from light's point of view
-//        simpleDepthShader.use();
-//        simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
-//
-//        glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
-//        glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-//        glClear(GL_DEPTH_BUFFER_BIT);
-//        glActiveTexture(GL_TEXTURE0);
-//        glBindTexture(GL_TEXTURE_2D, woodTexture);
-//        renderScene(simpleDepthShader);
-//        glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
         // reset viewport
         glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
-
-//        // render Depth map to quad for visual debugging
-//        // ---------------------------------------------
-//        debugDepthQuad.use();
-//        debugDepthQuad.setFloat("near_plane", near_plane);
-//        debugDepthQuad.setFloat("far_plane", far_plane);
-//        glActiveTexture(GL_TEXTURE0);
-//        glBindTexture(GL_TEXTURE_2D, depthMap);
-//        //renderQuad();
 
         // don't forget to enable shader before setting uniforms
         ourShader.use();
@@ -347,26 +270,16 @@ int main() {
         ourShader.setFloat("pointLight.quadratic", pointLight.quadratic);
         ourShader.setVec3("viewPosition", camera.Position);
         ourShader.setFloat("material.shininess", 32.0f);
+        ourShader.setInt("blinn", blinn);
+
+//        std::cout << (blinn ? "Blinn-Phong" : "Phong") << std::endl;
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),
-                                                (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 200.0f);
+                                                (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 300.0f);
         glm::mat4 view = camera.GetViewMatrix();
         ourShader.setMat4("projection", projection);
         ourShader.setMat4("view", view);
-
-//        ourShader.setInt("diffuseTexture", 0);
-//        ourShader.setInt("shadowMap", 1);
-//
-//
-//        ourShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
-//        glActiveTexture(GL_TEXTURE0);
-//        glBindTexture(GL_TEXTURE_2D, woodTexture);
-//        glActiveTexture(GL_TEXTURE1);
-//        glBindTexture(GL_TEXTURE_2D, depthMap);
-//
-//        debugDepthQuad.use();
-//        debugDepthQuad.setInt("depthMap", 0);
 
         //Face culling
         glEnable(GL_CULL_FACE);
@@ -407,17 +320,6 @@ int main() {
         dogMat=glm::scale(dogMat,glm::vec3 (3.0f));
         ourShader.setMat4("model", dogMat);
         _dog.Draw(ourShader);
-
-        //basketballHoop
-        glm::mat4 basketballHoopMat = glm::mat4(1.0f);
-        basketballHoopMat = glm::translate(basketballHoopMat,glm::vec3 (-89.78839874267578,
-                                                                        9.9052734375,
-                                                                        1.3269679546356201));
-        basketballHoopMat=glm::rotate(basketballHoopMat, glm::radians(70.0f), glm::vec3 (0.0f, 1.0f, 0.0f));
-        basketballHoopMat=glm::scale(basketballHoopMat,glm::vec3 (20.5f));
-
-        ourShader.setMat4("model", basketballHoopMat);
-        _basketballHoop.Draw(ourShader);
 
         //ball
         float maxHeight = 10.0f; // Maksimalna visina
@@ -543,6 +445,17 @@ void processInput(GLFWwindow *window) {
         camera.ProcessKeyboard(LEFT, deltaTime*10.5);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime*10.5);
+
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS && !blinnKeyPressed)
+    {
+        blinn = !blinn;
+        blinnKeyPressed = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_RELEASE)
+    {
+        blinnKeyPressed = false;
+    }
+
 }
 
 
